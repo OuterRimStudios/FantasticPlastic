3cffe408deedbf925aa445d98f6e9c367787a4d9
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-06-01 14:52:29 -0500

    Added hand controllers

---
+++ Assets/Scripts/ControllerTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerTracker : MonoBehaviour
+{
+    public enum Hand
+    {
+        Left,
+        Right
+    };
+
+    public Hand hand;
+
+    private void Update()
+    {
+        OVRInput.Update();
+        transform.localPosition = hand == Hand.Left ? OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch) : OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        transform.localRotation = hand == Hand.Left ? OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch) : OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+    }
+}
