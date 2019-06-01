c63e2432ccb64834a6da5af94199ef047253455d
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-06-01 14:25:26 -0500

    Added new caustics and volume lighting, added test animals

---
+++ Assets/Scripts/Caustics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Light))]
+public class Caustics : MonoBehaviour {
+    [SerializeField] List<Texture2D> cookies;
+    [SerializeField] float delayTime = 0.01f;
+    Light source;
+
+    private IEnumerator Start () {
+        source = GetComponent<Light> ();
+
+        for (int i = 0; i < cookies.Count; i++) {
+            source.cookie = cookies[i];
+            if (i == cookies.Count - 1)
+                i = 0;
+            yield return new WaitForSeconds (delayTime);
+        }
+    }
+}
