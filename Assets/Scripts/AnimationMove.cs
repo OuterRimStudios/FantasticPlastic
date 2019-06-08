570746649aa747fdf46067477734c890fb22c426
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-06-08 15:32:50 -0500

    Added animation movement

---
+++ Assets/Scripts/AnimationMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationMove : MonoBehaviour
+{
+    public float speed;
+    public float force;
+    public float acceleration;
+    public float deceleration;
+
+    float constantSpeed;
+
+    bool accelerating;
+
+    private void Start()
+    {
+        constantSpeed = speed;
+    }
+
+    private void Update()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+        if(!accelerating)
+            speed = Mathf.MoveTowards(speed, constantSpeed, deceleration * Time.deltaTime);
+    }
+
+    public void Accelerate()
+    {
+        StartCoroutine(Aceelerating());
+    }
+
+    IEnumerator Aceelerating()
+    {
+        accelerating = true;
+        yield return new WaitUntil(() => Accelerated());
+        speed = force;
+        accelerating = false;
+    }
+
+    bool Accelerated()
+    {
+        speed = Mathf.MoveTowards(speed, force, acceleration * Time.deltaTime);
+        return speed >= force;
+    }
+}
