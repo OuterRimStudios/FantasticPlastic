089ea4cdbf4bb7e8a774f6b162a87b67cc3b6473
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-06-14 08:11:51 -0500

    added some new colliders and new limb interaction

---
+++ Assets/InteractionLimb.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionLimb : Interaction
+{
+    public Interaction[] mainInteractions;
+
+    public override void Interact()
+    {
+        foreach (Interaction interaction in mainInteractions)
+            interaction.Interact();
+    }
+}
