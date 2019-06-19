44160b5c8537daf927b27309d37f4a12a103faf6
Jeff Fleetwood <JeffFleetwood@users.noreply.github.com>
2019-06-19 18:06:51 -0500

    Added albatross diving

---
+++ Assets/Asset Packages/Water_Splashes/Scripts/SmallSplash.js
@@ -0,0 +1,48 @@
+﻿#pragma strict
+
+var SmallSplash : GameObject;
+
+private var splashFlag = 0;
+
+
+function Start () 
+{
+
+    SmallSplash.SetActive(false);
+
+}
+
+function Update () {
+
+    if (Input.GetButtonDown("Fire2"))
+    {
+
+        if (splashFlag == 0)
+        {
+            TriggerSplash();
+        }
+       
+    }
+
+
+    
+}
+
+   
+function TriggerSplash()
+{
+    
+    splashFlag = 1;
+    
+    SmallSplash.SetActive(true);
+
+    yield WaitForSeconds (2.1);
+
+    SmallSplash.SetActive(false);
+
+    splashFlag = 0;
+
+}
+
+
+
