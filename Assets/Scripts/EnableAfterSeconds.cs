f2d4fdfee450b9c8ee94ed96a6f83d0f43fd00d6
Jeff Fleetwood <JeffFleetwood@users.noreply.github.com>
2019-06-19 18:39:18 -0500

    Added the albatrosses

---
+++ Assets/Scripts/EnableAfterSeconds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableAfterSeconds : MonoBehaviour
+{
+    public GameObject objectToEnable;
+    public float enableAfter;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(enableAfter);
+        objectToEnable.SetActive(true);
+    }
+}
