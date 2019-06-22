4d648662d96b72f44e3afcfbb39622a4628dda2c
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-06-22 15:00:01 -0500

    adjusted albatross splies

---
+++ Assets/Scripts/DisableAfter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableAfter : MonoBehaviour
+{
+    public float disableAfter = 2;
+
+    void OnEnable()
+    {
+        StartCoroutine(Disable());
+    }
+
+    IEnumerator Disable()
+    {
+        yield return new WaitForSeconds(disableAfter);
+        gameObject.SetActive(false);
+    }
+}
